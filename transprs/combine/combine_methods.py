1e547a6179c23a33b6d5ec61a7adde84b50bc017
duypham2108 <phamduychv94@gmail.com>
2021-09-03 01:24:43 +1000

    fix: store file rather than read directly

--- transprs/combine/combine_methods.py
+++ transprs/combine/combine_methods.py
@@ -18,9 +18,8 @@ def combine_methods(
     print("Adjusted BETA...")
     mixing_results = []
     for method in zip(methods, mixing_weight):
-        mixing_results.append(
-            processor.adjusted_ss[method[0]][use_col].values * method[1]
-        )
+        adjusted_ss = pd.read_table(processor.adjusted_ss[method[0]])
+        mixing_results.append(adjusted_ss[use_col].values * method[1])
 
     snps_list = []
     for method in methods:
@@ -30,11 +29,11 @@ def combine_methods(
 
     mixing_results = []
     for method in zip(methods, mixing_weight):
+
+        adjusted_ss = pd.read_table(processor.adjusted_ss[method[0]])
+
         mixing_results.append(
-            processor.adjusted_ss[method[0]][
-                processor.adjusted_ss[method[0]].SNP.isin(final_snps)
-            ][use_col].values
-            * method[1]
+            adjusted_ss[adjusted_ss.SNP.isin(final_snps)][use_col].values * method[1]
         )
 
     adjusted_beta = np.sum(mixing_results, axis=0)
@@ -46,13 +45,21 @@ def combine_methods(
     else:
         name_combine = "+".join(methods)
 
-    processor.adjusted_ss[name_combine] = processor.sumstats.copy()
+    sumstats = pd.read_table(processor.sumstats)
 
-    processor.adjusted_ss[name_combine] = processor.adjusted_ss[name_combine][
-        processor.adjusted_ss[name_combine].SNP.isin(final_snps)
+    adjusted_ss_combined = sumstats.copy()
+
+    adjusted_ss_combined = adjusted_ss_combined[
+        adjusted_ss_combined.SNP.isin(final_snps)
     ]
 
-    processor.adjusted_ss[name_combine][use_col] = adjusted_beta
+    adjusted_ss_combined[use_col] = adjusted_beta
+
+    save_path = processor.workdir + "/adjusted_sumstats_" + name_combine
+
+    adjusted_ss_combined.to_csv(save_path, sep="\t", index=False)
+
+    processor.adjusted_ss[name_combine] = save_path
 
     print("The clumping result stores in .adjusted_ss['" + name_combine + "']!")
 
