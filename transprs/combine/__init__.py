6a17ae800781ef71be2b8cbcc2f600d2948ef553
duypham2108 <phamduychv94@gmail.com>
2021-12-06 22:36:56 +1000

    fix: combine methods and prscsx

--- transprs/combine/__init__.py
+++ transprs/combine/__init__.py
@@ -1,3 +1,4 @@
 from .estimate_weighting import estimate_weighting
 from .estimate_weighting import estimate_weighting_multipop
-from .combine_multipop import combine_methods
+from .combine_methods import combine_multipop
+from . import multipop
